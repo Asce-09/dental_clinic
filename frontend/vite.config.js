261705f1aa6e660d348073642c7924394a58a7ae
Asce-09 <deitherpuente@gmail.com>
2026-09-10 16:34:37 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or your framework plugin
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      'aloe-overhead-commuting.ngrok-free.dev'
-    ]
-  }
+  plugins: [react()]
 })
